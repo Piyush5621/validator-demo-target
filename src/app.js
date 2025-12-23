81a4ff32629677d483f2b0cee54e3a39b2c10677
Piyush <piyushkumar10902080@gmai.com>
2025-12-23 15:25:18 +0530

    Initial verification setup

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("Starting GitOps Demo Application...");
+console.log("Connecting into database...");
+// TODO: Externalize credentials
+const dbConfig = {
+    host: "localhost",
+    port: 5432,
+    user: "admin"
+};
+console.log("App is running on port 3000");
